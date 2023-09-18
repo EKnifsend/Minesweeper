ddbd93355d9407d7c16af1b3bd51fab488e53fc1
eknifsend <knifsend@usc.edu>
2023-09-18 03:52:50 -0700

    Initial Commit - Initial Grid, Working Buttons, Working bomb generation and appropriate numbers, Working flag function

---
+++ app/src/main/java/com/example/minesweeper/Cell.java
@@ -0,0 +1,82 @@
+package com.example.minesweeper;
+
+import android.graphics.Color;
+import android.widget.TextView;
+import android.content.Context;
+
+public class Cell {
+
+    private Context context;
+
+    public final int ID;
+
+    public TextView tv;
+
+    private int adjacentBombs;
+    private boolean isBomb;
+    private boolean isFlagged = false;
+    private boolean revealed = false;
+
+    private String display;
+
+    public Cell (Context context, int id, boolean isBomb, int adjacentBombs, TextView tv) {
+        this.context = context;
+        this.ID = id;
+        this.isBomb = isBomb;
+        this.adjacentBombs = adjacentBombs;
+        this.tv = tv;
+
+        adjacentBombs = 1;
+
+        setDisplay();
+    }
+
+    private void setDisplay() {
+        if (isBomb) {
+            display = context.getString(R.string.mine);
+        }
+        else if (adjacentBombs > 0) {
+            display = String.valueOf(adjacentBombs);
+        }
+        else {
+            display = "";
+        }
+    }
+
+    public int getAdjacentBombs() {
+        return adjacentBombs;
+    }
+
+    public boolean isBomb() {
+        return isBomb;
+    }
+
+    public void flag() {
+        isFlagged = true;
+        tv.setText(context.getString(R.string.flag));
+    }
+
+    public void unflag() {
+        isFlagged = false;
+        tv.setText("");
+    }
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void changeText(String s) {
+        tv.setText(s);
+    }
+
+    public void reveal() {
+        revealed = true;
+
+        tv.setTextColor(Color.GRAY);
+        tv.setBackgroundColor(Color.LTGRAY);
+        tv.setText(display);
+    }
+}
---
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -0,0 +1,311 @@
+package com.example.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int COLUMN_COUNT = 10;
+    private static final int ROW_COUNT = 12;
+    private final int BOMBS = 4;
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<Cell> cells;
+
+    private ArrayList<Integer> bombLocations;
+
+    private int mode = 0;   // mode == 0 means they are selecting, mode == 1 means they are flagging
+
+    private int flags;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
+    private void updateFlagCount() {
+        TextView flagsCount = (TextView) findViewById(R.id.flagsCount);
+        flagsCount.setText(String.valueOf(flags));
+    }
+
+    private void createBombs() {
+        bombLocations = new ArrayList<Integer>(BOMBS);
+
+        Random random = new Random(System.currentTimeMillis());
+
+        int cellCount = COLUMN_COUNT * ROW_COUNT;
+        int bombsCreated = 0;
+        while (bombsCreated < BOMBS) {
+            // Generate a random number between 0 and cellCount
+            int location = random.nextInt(cellCount);
+
+            if (!bombLocations.contains(location)) {            // location is not already in array
+                bombLocations.add(location);
+                bombsCreated ++;
+            }
+        }
+    }
+
+    private int getRow (int id) {
+        return id/COLUMN_COUNT;
+    }
+
+    private int getCol (int id) {
+        return id%COLUMN_COUNT;
+    }
+
+    private int getId (int row, int col) {
+        if (row < 0 || row >= ROW_COUNT || col < 0 || col >= COLUMN_COUNT) {         // if row or
+            // column is out of bounds
+            return -1;
+        }
+        else {
+            return row * COLUMN_COUNT + col;
+        }
+    }
+
+    private int countAdjacentBombs (int id) {
+        int row = getRow(id);
+        int col = getCol(id);
+
+        int bombCount = 0;
+
+        //check upper left
+        if (bombLocations.contains(getId (row - 1, col - 1)))  {
+            bombCount ++;
+        }
+        //check upper
+        if (bombLocations.contains(getId (row - 1, col)))  {
+            bombCount ++;
+        }
+        //check upper right
+        if (bombLocations.contains(getId (row - 1, col + 1)))  {
+            bombCount ++;
+        }
+        //check right
+        if (bombLocations.contains(getId(row, col + 1)))  {
+            bombCount ++;
+        }
+        //check lower right
+        if (bombLocations.contains(getId(row + 1, col + 1)))  {
+            bombCount ++;
+        }
+        //check lower
+        if (bombLocations.contains(getId(row + 1, col)))  {
+            bombCount ++;
+        }
+        //check lower left
+        if (bombLocations.contains(getId(row + 1, col - 1)))  {
+            bombCount ++;
+        }
+        //check left
+        if (bombLocations.contains(getId(row, col - 1)))  {
+            bombCount ++;
+        }
+
+        return bombCount;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cells = new ArrayList<Cell>();
+
+        flags = BOMBS;
+        updateFlagCount();
+
+        // generate bomb locations
+        createBombs();
+
+        /*
+        // Method (1): add statically created cells
+        TextView tv00 = (TextView) findViewById(R.id.textView00);
+        TextView tv01 = (TextView) findViewById(R.id.textView01);
+        TextView tv10 = (TextView) findViewById(R.id.textView10);
+        TextView tv11 = (TextView) findViewById(R.id.textView11);
+
+        tv00.setTextColor(Color.GRAY);
+        tv00.setBackgroundColor(Color.GRAY);
+        tv00.setOnClickListener(this::onClickTV);
+
+        tv01.setTextColor(Color.GRAY);
+        tv01.setBackgroundColor(Color.GRAY);
+        tv01.setOnClickListener(this::onClickTV);
+
+        tv10.setTextColor(Color.GRAY);
+        tv10.setBackgroundColor(Color.GRAY);
+        tv10.setOnClickListener(this::onClickTV);
+
+        tv11.setTextColor(Color.GRAY);
+        tv11.setBackgroundColor(Color.GRAY);
+        tv11.setOnClickListener(this::onClickTV);
+
+        cell_tvs.add(tv00);
+        cell_tvs.add(tv01);
+        cell_tvs.add(tv10);
+        cell_tvs.add(tv11);
+         */
+
+        TextView bottomButton = (TextView) findViewById(R.id.bottomButton);
+        bottomButton.setOnClickListener(this::changeMode);
+
+        // Method (2): add four dynamically created cells
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COLUMN_COUNT; col++) {
+                TextView tv = new TextView(this);
+                tv.setHeight(dpToPixel(30));
+                tv.setWidth(dpToPixel(30));
+                tv.setTextSize(20);//dpToPixel(32) );
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GREEN);
+                tv.setOnClickListener(this::onClickTV);
+
+                int id = getId(row, col);
+                Cell c;
+
+                if (bombLocations.contains(id)) {           // id is listed as a bomb location
+                    c = new Cell(this, id, true, 0, tv);
+                }
+                else {
+                    int adjacentBombs = countAdjacentBombs(id);
+                    c = new Cell(this, id, false, adjacentBombs, tv);
+                }
+
+                cells.add(c);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(row);
+                lp.columnSpec = GridLayout.spec(col);
+
+                grid.addView(cells.get(id).tv, lp);
+            }
+        }
+
+        /*
+        // Method (3): add four dynamically created cells with LayoutInflater
+        LayoutInflater li = LayoutInflater.from(this);
+        for (int i = 4; i<=5; i++) {
+            for (int j=0; j<=1; j++) {
+                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
+                //tv.setText(String.valueOf(i)+String.valueOf(j));
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
+         */
+
+    }
+
+    private void revealAll() {
+        for (int row = 0; row < ROW_COUNT; row++) {
+            for (int col = 0; col < COLUMN_COUNT; col++) {
+                cells.get(getId(row, col)).reveal();
+            }
+        }
+    }
+
+    public void changeMode(View view){
+        TextView tv = (TextView) view;
+
+        if (mode == 0) {    // currently selecting
+            mode = 1;
+            tv.setText(getString(R.string.flag));
+        }
+        else {              // currently flagging
+            mode = 0;
+            tv.setText(getString(R.string.pick));
+        }
+    }
+
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cells.size(); n++) {
+            if ((cells.get(n)).tv == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    public void onClickTV(View view){
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        //int i = n/COLUMN_COUNT;
+        //int j = n%COLUMN_COUNT;
+        //tv.setText(String.valueOf(i)+String.valueOf(j));
+
+        Cell c = cells.get(n);
+        /*
+        if (tv.getCurrentTextColor() == Color.GREEN) {
+
+        }else {
+        }
+        */
+
+        if (mode==0) {
+            selectingAction(c);
+        }
+        else {
+            flaggingAction(c);
+        }
+    }
+
+    private void selectingAction(Cell c) {
+        if (!c.isFlagged()) {
+            if (c.isBomb()) {
+                destruction(c);
+            }
+            else {
+                c.reveal();
+            }
+        }
+    }
+
+    private void flaggingAction(Cell c) {
+        if (!c.isRevealed()) {          // can only flag if cell is not yet revealed
+            if (c.isFlagged()) {            // if c is flagged, unflag
+                c.unflag();
+                flags ++;
+                updateFlagCount();
+            }
+            else {                          // if c is unflagged, attempt to flag
+                if (flags > 0) {                // can only flag if you have available flags
+                    c.flag();
+                    flags --;
+                    updateFlagCount();
+                }
+            }
+        }
+    }
+
+    private void destruction(Cell c) {
+        c.reveal();
+        c.tv.setBackgroundColor(Color.RED);
+
+        // stop timer
+        revealAll();
+    }
+}
